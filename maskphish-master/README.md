20ec8f28044f931bf1b93a4abfd3f8097e7f86e4
HuzefaUsama25 <63582552+HuzefaUsama25@users.noreply.github.com>
2020-09-10 15:43:01 +0500

    Add files via upload

---
+++ maskphish-master/README.md
@@ -0,0 +1,28 @@
+<p align="center">
+	<img src="https://i.imgur.com/plp3lJu.jpg" width="600px" hight="100px">
+</p>
+
+## Author: https://github.com/jaykali
+## Twitter: https://twitter.com/KaliLinux_in
+
+
+### MaskPhish is a simple script to hide phishing URL under a normal looking URL(google.com or facebook.com).
+
+
+## Legal Disclaimer:
+Usage of MaskPhish for attacking targets without prior mutual consent is illegal. It's the end user's responsibility to obey all applicable local, state and federal laws. Developers assume no liability and are not responsible for any misuse or damage caused by this program
+
+## Installing (Tested on Kali Linux, Termux & Ubuntu):
+
+```
+git clone https://github.com/jaykali/maskphish
+cd maskphish
+./maskphish
+```
+## Detailed Article:
+https://www.kalilinux.in/2020/07/how-to-hide-phishing-link.html
+
+## Screenshot
+<p align="center">
+	<img src="https://i.imgur.com/1JsWv4I.png" width="600px">
+</p>
---
+++ maskphish-master/maskphish.sh
@@ -0,0 +1,51 @@
+# Bash Script for Hide Phishing URL Created by KP
+
+url_checker() {
+    if [ ! "${1//:*}" = http ]; then
+        if [ ! "${1//:*}" = https ]; then
+            echo -e "\e[31m[!] Invalid URL. Please use http or https.\e[0m"
+            exit 1
+        fi
+    fi
+}
+
+echo
+echo
+echo -e "\e[1;31;42m######┌──────────────────────────┐##### \e[0m"
+echo -e "\e[1;31;42m######│▙▗▌      ▌  ▛▀▖▌  ▗    ▌  │##### \e[0m"
+echo -e "\e[1;31;42m######│▌▘▌▝▀▖▞▀▘▌▗▘▙▄▘▛▀▖▄ ▞▀▘▛▀▖│##### \e[0m"
+echo -e "\e[1;31;42m######│▌ ▌▞▀▌▝▀▖▛▚ ▌  ▌ ▌▐ ▝▀▖▌ ▌│##### \e[0m"
+echo -e "\e[1;31;42m######│▘ ▘▝▀▘▀▀ ▘ ▘▘  ▘ ▘▀▘▀▀ ▘ ▘│##### \e[0m"
+echo -e "\e[1;31;42m######└──────────────────────────┘##### \e[0m"
+echo
+echo -e "\e[40;38;5;82m Please Visit \e[30;48;5;82m https://www.kalilinux.in \e[0m"
+echo -e "\e[30;48;5;82m    Copyright \e[40;38;5;82m   JayKali \e[0m"
+echo
+echo
+echo -e "\e[1;31;42m ### Phishing URL ###\e[0m"
+echo
+echo -n "Paste Phishing URL here (with http or https): "
+read phish
+url_checker $phish
+short=$(curl -s https://da.gd/s/?url=${phish})
+shorter=${short#https://}
+echo
+echo -e "\e[1;31;42m ### Masking Domain ###\e[0m"
+echo 'Domain to mask the Phishing URL (with http or https), ex https://google.com, http
+://anything.org) :'
+echo
+read mask
+url_checker $mask
+echo
+echo 'Type social engineering words:(like free-money, best-pubg-tricks)'
+echo
+echo -e "\e[31mDon't use space just use '-' between social engineering words\e[0m"
+echo
+read words
+echo
+echo 'Generating MaskPhish Link...'
+echo
+echo 'Here is the MaskPhish URL..'
+echo
+final=$mask-$words@$shorter
+echo $final
